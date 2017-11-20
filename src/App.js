22205698b3849a8ec6e03c84b715743ab8d819eb
Akinyemi Emmanuel <akinolu52@gmail.com>
2017-11-20 15:23:47 +0100

    geolocation and fb login

--- src/App.js
+++ src/App.js
@@ -1,102 +1,37 @@
 import React, { Component } from 'react';
-// import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
-import { Navbar, NavbarBrand, Form, Input, Card, CardTitle, CardText, CardDeck,
- CardBody, CardFooter } from 'reactstrap';
 import './App.css'
+import AuthComponent from './components/AuthComponent'
+import FindButtonComponent from './components/FindButtonComponent'
+import GeolocationComponent from './components/GeolocationComponent'
+import ListViewRenderComponent from './components/ListViewRenderComponent'
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
+  state = {
+    step: 1,
+  };
 
-  //   this.toggle = this.toggle.bind(this);
-  //   this.state = {
-  //     isOpen: false
-  //   };
-  // }
-  // toggle() {
-  //   this.setState({
-  //     isOpen: !this.state.isOpen
-  //   });
-  // }
+  nextStep = () => {
+    this.setState({
+      step: this.state.step + 1 
+    });
+  }
+
   render() {
-    return (
-      /*<div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">fnd app</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h3>fnd devs and dev events near you!</h3>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                fnd devs
-              </Button>
-            </p>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                fnd event
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
-      </div>*/
-      <div>
-        <Navbar color="faded" light expand="md" className="Header">
-          <NavbarBrand href="/">
-            <h2>FND APP</h2>
-          </NavbarBrand>
-        </Navbar>
-        <div className="ResultContainer">
-          <Form className="ResultForm">
-            <Input type="select" className="SearchCateria">
-              <option>Name</option>
-              <option>Location</option>
-            </Input>
-          </Form>
-          <div className="ResultWrapper">
-            <CardDeck>
-              <Card className="text-center">
-                <CardBody>
-                  <CardTitle></CardTitle>
-                  <CardText></CardText>
-                </CardBody>
-                <CardFooter></CardFooter>
-              </Card>
-              <Card className="text-center">
-                <CardBody>
-                  <CardTitle></CardTitle>
-                  <CardText></CardText>
-                </CardBody>
-                <CardFooter></CardFooter>
-              </Card>
-              <Card className="text-center">
-                <CardBody>
-                  <CardTitle></CardTitle>
-                  <CardText></CardText>
-                </CardBody>
-                <CardFooter></CardFooter>
-              </Card>
-            </CardDeck>
+    switch (this.state.step) {
+      case 1:
+        return (
+          <div>
+            <GeolocationComponent />
+            <AuthComponent nextStep={this.nextStep} />
           </div>
-          
-        </div>
-      </div>
-    );
+          )
+      case 2:
+        return <FindButtonComponent nextStep={this.nextStep} />
+      case 3:
+        return <ListViewRenderComponent nextStep={this.nextStep} />
+      default:
+        return null;
+    }
   }
 }
 
---
+++ src/components/AuthComponent.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import './Style.css'
+import HeaderComponent from './HeaderComponent';
+import { Button } from 'reactstrap';
+// import FontAwesome from 'react-fontawesome';
+// import faStyles from 'font-awesome/css/font-awesome.css';
+
+class AuthComponent extends Component {
+    componentDidMount(){
+            // Load the required SDK asynchronously for facebook, google and linkedin
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            
+            window.fbAsyncInit = function() {
+                window.FB.init({
+                  appId      : '1667800729926213',
+                  cookie     : true,  // enable cookies to allow the server to access the session
+                  xfbml      : true,  // parse social plugins on this page
+                  version    : 'v2.8' // use version 2.1
+                });
+            };
+        }
+        
+        facebookLogin = () => {
+            /*window.FB.login(
+                this.checkLoginState(), 
+                { scope : 'email, public_profile' } //Add scope whatever you need about the facebook user
+            ); */
+            
+            window.FB.login(
+                function(resp){
+                    this.statusChangeCallback(resp);
+                }.bind(this),{ scope : 'email,user_work_history,user_education_history,user_location,public_profile' });
+        }
+        
+        checkLoginState() {
+            alert("Checking Login Status")
+            console.log( "Checking login status..........." );
+            
+            window.FB.getLoginStatus(function(response) {
+                alert("FB Callback")
+                console.log("----------->")
+                console.log(response)
+                this.statusChangeCallback(response);
+            }.bind(this));
+        }
+        
+        statusChangeCallback(response) {
+            console.log('statusChangeCallback');
+            console.log(response);
+            if (response.status === 'connected') {
+                alert( "Connected to facebook. Retriving user from fb" );
+                // Logged into your app and Facebook.
+                this.fetchDataFacebook();
+            } else if (response.status === 'not_authorized') {
+                console.log('Import error', 'Authorize app to import data', 'error')
+            } else {
+                console.log('Import error', 'Error occured while importing data', 'error')
+            }
+        }
+        
+        fetchDataFacebook = () => {
+            console.log('Welcome!  Fetching your information.... ');
+
+            window.FB.api('/me', function(user) {
+                console.log( user );
+                console.log('Successful login from facebook : ' + user.name);
+                alert( 'Successful login for: ' + user.name );
+            });
+        }
+    
+    render() {
+        return (
+        	<div>
+        		<HeaderComponent />
+        		<div className="ComponentContainer">
+    				<Button onClick={ () => this.facebookLogin() }  className="AuthButton" color="primary"
+                    >
+        	    		Auth With Facebook
+    	    		</Button>
+                    <Button onClick={this.props.nextStep} className="AuthButton" color="info">
+                        Next
+                    </Button>
+        		</div>
+        	</div>
+        );
+      }
+}
+
+export default AuthComponent;
