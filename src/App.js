f7f33fddbe350ae389eacab2d779ec80812e8713
emmaodia <odia.emma@gmail.com>
2018-08-15 03:42:00 +0100

    Set state and componentDidMount.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-state = {
-  gists = []
-}
+
 
 class App extends Component {
+
+      state = {
+        gists = []
+      }
+
+      componentDidMount() {
+      axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+          const persons = res.data;
+          this.setState({ persons });
+        })
+    }
+
     render(){
       return(
         <div>THis works</div>
