324686f43a214fd369345ed9463d126a8ab80136
emmaodia <odia.emma@gmail.com>
2018-08-15 03:31:38 +0100

    Reset app to begin building from scratch!

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
-import './App.css'
-//import AuthComponent from './components/AuthComponent'
-import FindButtonComponent from './components/FindButtonComponent'
-import GeolocationComponent from './components/GeolocationComponent'
-import ListViewRenderComponent from './components/ListViewRenderComponent'
+import './App.css';
 
 class App extends Component {
-  state = {
-    step: 1,
-  };
-
-  nextStep = () => {
-    this.setState({
-      step: this.state.step + 1
-    });
-  }
-
-  render() {
-    switch (this.state.step) {
-      case 1:
-        return (
-          <div>
-            <GeolocationComponent />
-            {/*<AuthComponent nextStep={this.nextStep} />*/}
-          </div>
-          )
-      case 2:
-        return <FindButtonComponent nextStep={this.nextStep} />
-      case 3:
-        return <ListViewRenderComponent nextStep={this.nextStep} />
-      default:
-        return null;
+    render(){
+      return(
+        <div>THis works</div>
+      );
     }
-  }
 }
 
 export default App;
